5362d1798196a301276d31e3dff8690ca35a25d8
skleinjung <skleinjung@gmail.com>
2016-10-16 02:51:34 -0500

    Added primitive routing and recipe detail display.

--- recipes-web/src/main/resources/static/js/app.js
+++ recipes-web/src/main/resources/static/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("recipes-app", []);
+var app = angular.module("recipes-app", ['ngRoute']);
 
 app.controller("RecipesController", function($scope, $http) {
     $http.get('/api/recipes').
@@ -8,4 +8,28 @@ app.controller("RecipesController", function($scope, $http) {
     error(function(data, status, headers, config) {
         // log error
     });
+});
+
+app.controller('RecipeController', function($scope, pageData) {
+    $scope.name = 'RecipeController';
+    //$scope.params = $routeParams;
+    $scope.recipe = pageData;
+});
+
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/:recipeId/:slug', {
+            templateUrl: 'recipes/detail.html',
+            controller: 'RecipeController',
+            resolve: {
+                pageData: function($http, $route) {
+                    return $http.get('/api/recipes/' + $route.current.params.recipeId)
+                        .then(function(response) {
+                            return response.data;
+                        });
+                }
+            }
+        });
+
+    $locationProvider.html5Mode(true);
 });
